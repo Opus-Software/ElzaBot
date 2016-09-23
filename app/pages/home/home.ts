1bce449a03070bd90899cacf43ba46aa816a812f
thiago.toledo <thiago.toledo@opus-software.com.br>
2016-09-23 11:11:45 -0300

    - Add the final  button and the background image

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,32 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
-
+import { Component } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { NavController, Button } from 'ionic-angular';
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  directives: [ Button ]
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
+  token: string = 'xoxb-83005887287-ejuGtQcP0Fj0PpTKwTeXFrhr';
+  channel: string = 'elzabot_test';
+  text: string = 'Amanhã teremos café';
+  as_user: boolean = true;
+  slackUrl: string = 'https://slack.com/api/chat.postMessage';
+
+  header: Headers;
   
+  constructor(private navCtrl: NavController,
+              private http: Http) {
+      this.header = new Headers();
+      this.header.append('Content-Type', 'application/x-www-form-urlencoded');
+  }
+
+  screenForCoffee() {
+
+    let body = `token=${this.token}&channel=${this.channel}&text=${this.text}&as_user=${this.as_user}`;
+
+    let url = this.slackUrl;
+
+    this.http.post(url, body, { headers: this.header })
+      .subscribe(data => console.log(data));
   }
 }
