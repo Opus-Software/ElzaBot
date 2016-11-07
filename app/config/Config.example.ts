56e3bf72bae1c11c2672e58fea7764c43d151a2a
ggdaltoso <ggdaltoso@gmail.com>
2016-11-07 10:38:30 -0200

    Add config

---
+++ app/config/Config.example.ts
@@ -0,0 +1,6 @@
+export class Config {
+    static token: string = 'TOKEN';
+    static channel: string = 'CHANNEL_NAME';
+
+    static text: string = 'TEXT';
+}
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { NavController, Button } from 'ionic-angular';
 import { Toast } from 'ionic-native';
+import { Config } from '../../config/Config';
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
   directives: [ Button ]
 })
 export class HomePage {
-  token: string = 'xoxb-83005887287-ejuGtQcP0Fj0PpTKwTeXFrhr';
-  channel: string = 'cafe';
-  text: string = 'Oi gente, o café está pronto e quente! :coffee:';
+  token: string = Config.token;
+  channel: string = Config.channel;
+  text: string = Config.text;
+  
   as_user: boolean = true;
   slackUrl: string = 'https://slack.com/api/chat.postMessage';
   timeout: boolean = false;
